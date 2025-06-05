4d3e3c96df5aeb2c99d7a70ec2492a89107fe482
Neeshu Kumar <kumarneeshu7409@gmail.com>
2025-06-05 10:45:43 +0530

    Initial Commit

---
+++ Hanuman PDF Reader/Hanuman_PDF_ReaderApp.swift
@@ -0,0 +1,20 @@
+//
+//  Hanuman_PDF_ReaderApp.swift
+//  Hanuman PDF Reader
+//
+//  Created by Neeshu Kumar on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Hanuman_PDF_ReaderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
